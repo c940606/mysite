086881c823b08da0c7ebc9090c11c807537650f2
powcai <762307667@qq.com>
2022-12-04 14:27:52 +0800

    update

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,8 +6,11 @@ import ElementPlus from "element-plus";
 import zhCn from "element-plus/es/locale/lang/zh-cn"; // 中文
 import "element-plus/dist/index.css"; // 样式文件
 
+import vue3videoPlay from 'vue3-video-play' // 引入组件
+import 'vue3-video-play/dist/style.css' // 引入css
 const app = createApp(App);
 app
+  .use(vue3videoPlay)
   .use(createPinia())
   .use(ElementPlus, { locale: zhCn })
   .mount("#app");
